4fcfb0dc3d1e94994dbfceeab392c1252fd0c788
shiva <shiva.nelikanti@gmail.com>
2022-04-26 19:07:20 +0530

    Committing as new Project

---
+++ src/main/java/AFirstProjectEclipseToGitHub/AFirstProjectEclipseToGitHub/App.java
@@ -0,0 +1,13 @@
+package AFirstProjectEclipseToGitHub.AFirstProjectEclipseToGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
